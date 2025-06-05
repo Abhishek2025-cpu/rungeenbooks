f86be2e28016d3f228ce10a90f5d1285bd95b717
Abhishek <abhisheks@pearlorganisation.com>
2025-06-05 16:55:29 +0530

    test2

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -2,16 +2,21 @@ const express = require('express');
 const router = express.Router();
 const bookController = require('../Controllars/bookController');
 const multer = require('multer');
+
+
+// Setup multer
 const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
-const upload = multer({ storage }).fields([
-  { name: 'images', maxCount: 5 },
-  { name: 'pdf', maxCount: 1 }
-]);
-
-
-// Routes
-router.post('/add-books', upload.array('images'), bookController.addBook);
+// Route: Add a new book
+router.post(
+  '/add-books',
+  upload.fields([
+    { name: 'images', maxCount: 10 },
+    { name: 'pdf', maxCount: 1 }
+  ]),
+  bookController.addBook
+);
 router.get('/get-books/category/:categoryId', bookController.getBooksByCategory);
 router.get('/get-book/:bookId', bookController.getBookById);
 router.patch('/like-book/:bookId/toggle-like', bookController.toggleLike);
