17f76e0125b74ba8b5f86a79604ca32beccf3f62
abhishek <abhishekksharmaa11@gmail.com>
2025-06-08 16:33:51 +0530

    text

--- Routes/BooksRoute.js
+++ Routes/BooksRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { addBook, getBooksByCategory } = require('../Controllers/booksController');
-const upload = require('../middlewares/Multerconfig'); // your multer setup
+const { addBook, getBooksByCategory } = require('../Controllars/booksController');
+const upload = require('../middlewares/Multerconfig'); // your multer setup done
 
 router.post('/add-book', upload.array('images'), addBook);
 router.get('/category/:categoryId', getBooksByCategory);
