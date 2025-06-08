75ba9059203035a92d1cd8ffb3c7055ef9b84b38
abhishek <abhishekksharmaa11@gmail.com>
2025-06-08 11:25:36 +0530

    test2

--- Models/book.js
+++ Models/book.js
@@ -1,33 +1,21 @@
+// Models/book.js
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
-  categoryId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Category',
-    required: true,
-  },
-  bookName: {
-    type: String,
-    required: true,
-  },
-  author: {
-    type: String,
-    required: true,
-  },
-  about: {
-    type: String,
-  },
+  name: { type: String, required: true },
+  author: { type: String, required: true },
+  about: { type: String },
   status: {
     type: String,
     enum: ['available', 'unavailable'],
     default: 'available',
   },
-  images: [
-    {
-      filename: String,
-      path: String,
-    },
-  ],
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+  images: [String], // Path to uploaded images
 }, { timestamps: true });
 
-module.exports = mongoose.model('Book', bookSchema);
+module.exports = mongoose.models.Book || mongoose.model('Book', bookSchema);
