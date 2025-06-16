3a6cd7ef7fcee07b595257e38693902188f7d1a0
Abhishek <abhisheks@pearlorganisation.com>
2025-06-16 16:38:48 +0530

    like

--- Models/Like.js
+++ Models/Like.js
@@ -1,16 +1,8 @@
-
 const mongoose = require('mongoose');
+
 const likeSchema = new mongoose.Schema({
-  bookId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Book',
-    required: true,
-  },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-}, { timestamps: true });
+  bookId: { type: mongoose.Schema.Types.ObjectId, ref: 'Book', required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+});
 
 module.exports = mongoose.model('Like', likeSchema);
