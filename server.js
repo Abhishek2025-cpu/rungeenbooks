a896b483ff325d27cfe048659cc67a4b3781fa31
Abhishek <abhisheks@pearlorganisation.com>
2025-07-07 17:44:25 +0530

    file size

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ app.use(cors({
   origin: '*', // Allow all origins
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
 }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 
 app.use('/uploads', express.static('uploads')); // serve uploaded PDFs
 
