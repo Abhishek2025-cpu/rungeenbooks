aae199ee15edabd68a358d5b6d4c3e91fbf1067a
Abhishek <abhisheks@pearlorganisation.com>
2025-07-09 17:28:15 +0530

    test3

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 
 const categoryRoutes = require('./Routes/categoryRoutes');
 const authRoutes = require('./Routes/authRoutes');
-const bookRoutes = require('./Routes/bookRoutes');
+
 const bookAction = require("./Routes/bookActions");
 const bookRoute = require('./Routes/books');
 
@@ -32,7 +32,7 @@ app.use('/uploads', express.static('uploads')); // serve uploaded PDFs
 app.use(express.json());
 app.use(authRoutes);
 app.use('/api/categories', categoryRoutes);
-app.use('/api/books', bookRoutes);
+
 app.use('/api/book', bookAction);
 app.use('/api/books', bookRoute);
 // 404 handler
