54b7f0a1b57b70526e1a3e1f725509e55123d678
Abhishek <abhisheks@pearlorganisation.com>
2025-07-09 18:27:45 +0530

    test7

--- server.js
+++ server.js
@@ -26,8 +26,9 @@ if (!fs.existsSync(uploadsDir)) {
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 
+const uploadsAbsolutePath = path.resolve(__dirname, 'uploads');
+app.use('/uploads', express.static(uploadsAbsolutePath));
 
-app.use('/uploads', express.static('uploads')); // serve uploaded PDFs
 app.use(express.json());
 app.use(authRoutes);
 app.use('/api/categories', categoryRoutes);
