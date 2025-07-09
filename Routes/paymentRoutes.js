eac345bd0cd88f50b4d8b51de673a4e90701c6cd
Abhishek <abhisheks@pearlorganisation.com>
2025-07-09 19:41:55 +0530

    payment routes

--- Routes/paymentRoutes.js
+++ Routes/paymentRoutes.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const { verifyPayment } = require('../controllers/paymentController');
+const { verifyPayment } = require('../Controllars/paymentController');
 
 router.post('/verify', verifyPayment);
 
