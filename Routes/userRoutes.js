d49f02752645f6af0fe672504cae08e11cf66b53
abhishek <abhishekksharmaa11@gmail.com>
2025-07-25 22:37:02 +0530

    upload

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { updateUserProfile } = require('../Controllars/authController'); // or userController
 const upload = require('../middlewares/multer'); // import multer config
 
-router.put('/update-profile/:userId', upload.single('profileImage'), updateUserProfile);
+router.put('/update-profile/:userId', upload, updateUserProfile);
 
 module.exports = router;
