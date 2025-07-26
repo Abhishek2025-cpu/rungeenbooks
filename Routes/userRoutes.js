d0484ebf6599db011a693c1fe3a0aebad42dc7c0
Abhishek <abhisheks@pearlorganisation.com>
2025-07-26 15:34:49 +0530

    user controllar

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const uploadProfile = require('../middlewares/uploadProfile'); // ✅ use cloudinary uploader
-const userController = require('../Controllers/userController'); // ✅ also fix "Controllars" to "Controllers"
+const userController = require('../Controllars/userController'); // ✅ also fix "Controllars" to "Controllers"
 
 router.get('/get', userController.getAllUsers);
 router.put('/update/:id', uploadProfile.single('profileImage'), userController.updateUser);
