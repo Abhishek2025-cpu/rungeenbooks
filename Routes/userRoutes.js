a71ca04807b9b847d4fd7417065545a0c14c4c68
Abhishek <abhisheks@pearlorganisation.com>
2025-07-26 14:06:14 +0530

    controllar

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const upload = require('../middlewares/uploadProfile');
+const upload = require('../middlewares/uploadProfile'); // updated path
 const userController = require('../Controllars/userController');
 
 router.get('/get', userController.getAllUsers);
-router.put('/update/:id', upload.single('profileImage'), userController.updateUser); // ✅ this line must have valid functions
+router.put('/update/:id', upload.single('profileImage'), userController.updateUser);
 router.delete('/delete/:id', userController.deleteUser);
 router.get('/single/:id', userController.getUserById);
-
 module.exports = router;
 
