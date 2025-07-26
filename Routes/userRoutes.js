fb93df653d4ac348b0d48ebece4440f8fc2fa98f
Abhishek <abhisheks@pearlorganisation.com>
2025-07-26 14:03:52 +0530

    upload single image

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const upload = require('../middlewares/uploadProfile'); // updated path
+const upload = require('../middlewares/uploadProfile');
 const userController = require('../Controllars/userController');
 
 router.get('/get', userController.getAllUsers);
-router.put('/update/:id', upload.single('profileImage'), userController.updateUser);
+router.put('/update/:id', upload.single('profileImage'), userController.updateUser); // ✅ this line must have valid functions
 router.delete('/delete/:id', userController.deleteUser);
 router.get('/single/:id', userController.getUserById);
+
 module.exports = router;
 
