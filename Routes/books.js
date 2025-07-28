ea07d3b7a8aadf03d1d530396b7a9d3822752855
Abhishek <abhisheks@pearlorganisation.com>
2025-07-28 13:56:24 +0530

    cheques

--- Routes/books.js
+++ Routes/books.js
@@ -7,7 +7,7 @@ const bookContr = require('../Controllars/booksController');
 router.post('/add-book', upload, bookContr.addBook);
 router.get('/get-books/category/:categoryId', bookContr.getBooksByCategory);
 router.get('/single/:id', bookContr.getBookById);
-router.put('/update/:id', upload.fields([{ name: 'pdf' }, { name: 'coverImage' }]), bookContr.updateBook);
+router.put('/update/:id', upload, bookContr.updateBook);
 router.delete('/delete/:id', bookContr.deleteBook);
 router.patch('/toggle/:id', bookContr.toggleBookStatus);
 
