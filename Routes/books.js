ab15636535495d03e5a774d248e23d03c41a6c35
Abhishek <abhisheks@pearlorganisation.com>
2025-07-28 13:44:30 +0530

    update delete toggle book apis

--- Routes/books.js
+++ Routes/books.js
@@ -7,9 +7,9 @@ const bookContr = require('../Controllars/booksController');
 router.post('/add-book', upload, bookContr.addBook);
 router.get('/get-books/category/:categoryId', bookContr.getBooksByCategory);
 router.get('/single/:id', bookContr.getBookById);
-router.put('/update/:id', upload.fields([{ name: 'pdf' }, { name: 'coverImage' }]), bookController.updateBook);
-router.delete('/delete/:id', bookController.deleteBook);
-router.patch('/toggle/:id', bookController.toggleBookStatus);
+router.put('/update/:id', upload.fields([{ name: 'pdf' }, { name: 'coverImage' }]), bookContr.updateBook);
+router.delete('/delete/:id', bookContr.deleteBook);
+router.patch('/toggle/:id', bookContr.toggleBookStatus);
 
 
 module.exports = router;
