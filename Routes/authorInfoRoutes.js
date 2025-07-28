8bef3adc886e00119514b0aef1c3db62013f85a6
Abhishek <abhisheks@pearlorganisation.com>
2025-07-28 10:55:07 +0530

    .

--- Routes/authorInfoRoutes.js
+++ Routes/authorInfoRoutes.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const upload = require('../middlewares/uploadProfile');
+const uploadProfile = require('../middlewares/uploadProfile');
 const {
   addAuthor,
   getAuthors,
   updateAuthor
 } = require('../Controllars/authorInfoController');
 
-router.post('/add', upload, addAuthor);
+router.post('/add', uploadProfile.single('profile'), addAuthor);
 router.get('/get', getAuthors);
-router.patch('/update/:id', upload, updateAuthor);
-
+router.patch('/update/:id', uploadProfile.single('profile'), updateAuthor);
 module.exports = router;
