aac50b24f55823e4b12582981e6941d76dd96419
Abhishek <abhisheks@pearlorganisation.com>
2025-08-21 17:50:59 +0530

    verify

--- Controllars/orderController.js
+++ Controllars/orderController.js
@@ -61,7 +61,7 @@ exports.createOrder = async (req, res) => {
     res.status(200).json({
       success: true,
       message: "Order created successfully",
-      keyId: "rzp_test_WFobdSiykj0jlI", // <-- Send the public key to the client
+     
       razorpayOrderId: razorpayOrder.id,
       amount: amountInPaise,
       currency: "INR",
